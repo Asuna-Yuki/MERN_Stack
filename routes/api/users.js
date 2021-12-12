2c01c5059e46127b2731d9ec3e8bf7116ce38d29
Asuna-Yuki <rahulraj.sagg99@gmail.com>
2021-12-13 01:09:22 +0530

    added gravatar encrypted password

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,11 +1,86 @@
 const express = require("express");
 const router = express.Router();
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const { check, validationResult } = require("express-validator");
 
-// @route  GET api/users
-// @des    test route
+//User model
+const User = require("../../models/User");
+
+// @route  POST api/users
+// @des    regiser user
 // @access Public
-router.get("/", (req, res) => {
-  res.send("user route");
-});
+router.post(
+  "/",
+  [
+    check("name", "name is required").not().isEmpty(),
+    check("email", "Enter a Valid email").isEmail(),
+    check(
+      "password",
+      "Please enter a password with 8 or more characters"
+    ).isLength({ min: 8 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, password } = req.body;
+
+    try {
+      //see if the user exists
+      let user = await User.findOne({ email: email });
+
+      if (user) {
+        res.status(400).json({ errors: [{ msg: "User already exists" }] });
+      }
+
+      //get user gravatar
+      const avatar = gravatar.url(email, {
+        s: "200",
+        r: "pg",
+        d: "mm",
+      });
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password,
+      });
+
+      //encrypt password
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      //return jsontoken
+      const payLoad = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payLoad,
+        config.get("jwtSecret"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("server error");
+    }
+  }
+);
 
 module.exports = router;
