69aca9c411152d94ec7bc52729b47c8a220a01b0
Asuna-Yuki <rahulraj.sagg99@gmail.com>
2021-12-29 19:31:43 +0530

    added register model

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,25 @@
 import React, { Fragment } from "react";
 import Navbar from "./components/layout/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Landing from "./components/layout/Landing";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
 import "./App.css";
 
 const App = () => {
   return (
-    <Fragment>
+    <Router>
       <Navbar />
-      <Landing />
-    </Fragment>
+      <Routes>
+        <Route exact path='/' element={<Landing />}></Route>
+      </Routes>
+      <section className='container'>
+        <Routes>
+          <Route path='/login' element={<Login />}></Route>
+          <Route path='/register' element={<Register />}></Route>
+        </Routes>
+      </section>
+    </Router>
   );
 };
 
