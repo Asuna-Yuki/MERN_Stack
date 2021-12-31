5fbcc56ff998fafce5e26806925e5adc558850ff
Asuna-Yuki <rahulraj.sagg99@gmail.com>
2021-12-31 20:32:56 +0530

    added redux store

--- client/src/App.js
+++ client/src/App.js
@@ -5,21 +5,28 @@ import Landing from "./components/layout/Landing";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 import "./App.css";
+// Redux
+import { Provider } from "react-redux";
+import store from "./store";
 
 const App = () => {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route exact path='/' element={<Landing />}></Route>
-      </Routes>
-      <section className='container'>
-        <Routes>
-          <Route path='/login' element={<Login />}></Route>
-          <Route path='/register' element={<Register />}></Route>
-        </Routes>
-      </section>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Routes>
+            <Route path='/' element={<Landing />} exact></Route>
+          </Routes>
+          <section className='container'>
+            <Routes>
+              <Route path='/login' element={<Login />}></Route>
+              <Route path='/register' element={<Register />}></Route>
+            </Routes>
+          </section>
+        </Fragment>
+      </Router>
+    </Provider>
   );
 };
 
