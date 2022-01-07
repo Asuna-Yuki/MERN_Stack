620cb59c42afaa07e95ac2227f8b6c380e5f243f
Asuna-Yuki <rahulraj.sagg99@gmail.com>
2022-01-07 20:22:01 +0530

    created utiles and register and load user function

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,28 @@
-import React, { Fragment } from "react";
-import Navbar from "./components/layout/Navbar";
+import React, { Fragment, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 import Alert from "./components/layout/Alert";
+import setAuthToken from "./utils/setAuthToken";
+import { loadUser } from "./actions/auth";
 
 // Redux
 import { Provider } from "react-redux";
 import store from "./store";
 
 import "./App.css";
+
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
+
 const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
   return (
     <Provider store={store}>
       <Router>
